31020037a8d932c9d31cc4edf64a1575ac1b8bc1
MBSAL <97360715+MBSAL@users.noreply.github.com>
2022-03-07 16:06:36 -0500

    Update HP_mode.c

--- Functions/HP_mode.c
+++ Functions/HP_mode.c
@@ -31,9 +31,13 @@ dim SAMPLE_RATE : SAMPLE_RATE = 500   //ADC sample rate in Hz
 
 
 
-function HP_mode(BYVAL CURRENT_SET_DC AS INTEGER)  
-
+sub HP_MODE_ROUTINE(BYVAL CURRENT_SET_DC AS INTEGER)  
+	CANCEL_ALL_TIMERS()
+//	TimerStart (3,Battery_Check_Interval,1)
+	OUTPUT_STATE(MODE_HP)
+	print "\n HP_MODE state start \n"
     OPTICAL_TX(CURRENT_SET_DC) //start TX with desired current
+	print "\n OPTICAL_TX  FINISH \n"
 
     dim START_TIME   //current tick value for delay (ms)
     dim END_TIME     //final tick value for delay   (ms)
@@ -42,35 +46,41 @@ function HP_mode(BYVAL CURRENT_SET_DC AS INTEGER)
     dim CURR_BLINK : CURR_BLINK = 0   //tick time of the current blink (ms), if there is one.
     
 
-    do  //main loop
-
+  //  do  //main loop
+		//print "\n adc sample loop start \n"
         ADC_RAW = GPIOREAD(ENV_ADC)  //Get one sample
         //convert current ADC_RAW sample to mV and add to array or circular buffer?
         //BLE transmit??
         START_TIME = GETTICKCOUNT()  //Get initial tick in ms
-        END_TIME = START_TIME + (1/SAMPLE_RATE)*1000   //Calculate end time based on the sample rate (ms)
-        
+        END_TIME = START_TIME + 2//((1/ SAMPLE_RATE)*1000)   //Calculate end time based on the sample rate (ms)
+       // print "\n start time: "; START_TIME
+		//print "\n end time: "; END_TIME
+        TimerStart(4,END_TIME,1)
 
         WHILE GETTICKCOUNT() < END_TIME           
-
+			//print "\n \n delay loop start \n"
             if PREV_BLINK == 0 then      //Only runs on first detected blink.
-
-                if GPIOREAD(SLOPE_P) == 1 then     //Sets first blink to previous if SLOPE_P comparator high
+				
+                if GPIOREAD(SLOPE_P) == 0 then     //Sets first blink to previous if SLOPE_P comparator high
                     PREV_BLINK = GETTICKCOUNT()
                     PRINT "\n Init Blink time: ";PREV_BLINK;"ms \n"
                 endif
 
             else                         //All consecutive blinks
-
-                if (GPIOREAD(SLOPE_P) == 1)&((GETTICKCOUNT()-PREV_BLINK)>200)   //if SLOPE_P high AND the time since the previous blink is greater than 200ms
+				if GETTICKCOUNT() - PREV_BLINK >= TARGET_BLINK_INTERVAL then // if interval time exceeded, use haptics 
+					PRINT "\n Interval time exceeed, haptics used \n"
+					PREV_BLINK = GETTICKCOUNT()
+				ENDIF
+                if (GPIOREAD(SLOPE_P) == 0)&((GETTICKCOUNT()- PREV_BLINK) > MIN_TIME_BTWN_BLINKS) then  //if SLOPE_P high AND the time since the previous blink is greater than 200ms
                     CURR_BLINK = GETTICKCOUNT()
                     //Store current blink time data?
                     //BLE transmit blink time data?
 
-                    PRINT "\n PREV_BLINK @ ";PREV_BLINK;"ms \n"
+                   // PRINT "\n PREV_BLINK @ ";PREV_BLINK;"ms \n"
                     PRINT "\n CURR_BLINK @ ";CURR_BLINK;"ms \n"
                     PREV_BLINK = CURR_BLINK
-
+				else
+					
                 endif
 
             endif
@@ -78,8 +88,8 @@ function HP_mode(BYVAL CURRENT_SET_DC AS INTEGER)
 
         ENDWHILE 
 
-    dowhile PBSTAT == 0    //run loop until switch is pressed once
+ //   dowhile GPIOREAD(PBSTAT) == 1    //run loop until switch is pressed once
+//	OPTICAL_TX(0)
+//	print "\n exiting HP Mode \n"
 
-
-
-endfunc
+endsub
